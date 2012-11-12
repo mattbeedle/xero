a3562d1ce2fff54c8825258288771f7d45c83a8a
Matt Beedle <mattbeedle@googlemail.com>
2012-11-12 19:47:27 +0100

    Switch logging level to info

--- lib/xero/connection.rb
+++ lib/xero/connection.rb
@@ -46,7 +46,7 @@ module Xero
 
       headers = { 'charset' => 'utf-8' }
 
-      Xero.configuration.logger.debug(
+      Xero.configuration.logger.info(
         "XERO::Connection #{method.upcase} #{uri} with #{params}"
       )
 
@@ -56,7 +56,7 @@ module Xero
         response = access_token.post(uri, params, headers)
       end
 
-      Xero.configuration.logger.debug(
+      Xero.configuration.logger.info(
         "XERO::Connection RESPONSE code: #{response} body: #{response.body}"
       )
 
